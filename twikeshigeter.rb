ce212f5e6b8081be61973ea9cf7a70d62dede3e6
taroooyan <taroooyan>
2016-02-22 22:54:53 +0900

    TweetしたUserを取得、表示するようにした

--- twikeshigeter.rb
+++ twikeshigeter.rb
@@ -19,12 +19,20 @@ def following(client)
 end
 
 
-def home_timeline(client)
-  # timeline of specific user
-  # search = client.user_timeline(USERNAME, { count: 100, since: 2016-02-20 })
-  search = client.home_timeline(count: 100)
-  search.each do |tweet|
-    puts "#{tweet.created_at.strftime("%Y/%m/%d %X")}: #{tweet.text}"
+def home_timeline(client, date_b)
+  begin
+    # timeline of specific user
+    # search = client.user_timeline(USERNAME, { count: 100, since: 2016-02-20 })
+    search = client.home_timeline(count: 100)
+    search.each do |tweet|
+      if date_b.strftime("%Y/%m/%d %X") < tweet.created_at.strftime("%Y/%m/%d %X")
+        puts "#{tweet.created_at.strftime("%Y/%m/%d %X")}: #{tweet.text} :by @#{tweet.user.screen_name}"
+        # puts tweet.methods
+      end
+    end
+  rescue Twitter::Error::TooManyRequests => error
+    sleep error.rate_limit.reset_in
+    retry
   end
 end
 
@@ -38,10 +46,24 @@ def main
     config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
   end
 
-  following_users = following(client)
-  following_users.each_with_index do|user, i|
-    puts "#{i+1}: #{user.screen_name}"
+  # following_users = following(client)
+  # following_users.each_with_index do|user, i|
+  #   puts "#{i+1}: #{user.screen_name}"
+  # end
+  date_b = Time.now
+  while true do
+    date_a = Time.now
+    if (date_a-date_b).to_i > 60
+      # p date_b
+      # p date_a
+      home_timeline(client, date_b)
+      date_b = date_a
+    end
+    # if date.strftime()
+    # p Time.now.strftime("%Y/%m/%d %X")
+    sleep(10)
   end
+
   
 end
 
