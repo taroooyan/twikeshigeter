019e1e491e0ad6987a8ed1f50e8d419f1bb491f8
taroooyan <taroooyan>
2016-02-25 01:27:45 +0900

    リファクタリング

--- twikeshigeter.rb
+++ twikeshigeter.rb
@@ -3,26 +3,12 @@ require 'twitter'
 require 'date'
 require 'dotenv'
 require 'slack'
-$stdout.sync = true
+
 # Ctrl + C で停止させられた場合の処理を登録
 Signal.trap(:INT){
-    puts " SIGINT"
-      exit(0)
+  puts " SIGINT"
+  exit(0)
 }
-
-
-def progress_bar(i, max = 100)
-  i = max if i > max
-  rest_size = 1 + 5 + 1      # space + progress_num + %
-  bar_width = 79 - rest_size # (width - 1) - rest_size = 72
-  percent = i * 100.0 / max
-  bar_length = i * bar_width.to_f / max
-  bar_str = ('#' * bar_length).ljust(bar_width)
-#  bar_str = '%-*s' % [bar_width, ('#' * bar_length)]
-  progress_num = '%3.1f' % percent
-  print "\r#{bar_str} #{'%5s' % progress_num}%"
-end
-
 
 # send to slack of #deleted-tweet channel
 def notice_slack(message)
@@ -33,155 +19,122 @@ def notice_slack(message)
   Slack.chat_postMessage(text: message, channel: '#deleted-tweet')
 end
 
-
 # user別にtxtファイルに保存
-def save(username, text)
-  File::open("#{username}.txt", "a") do |file|
+def save(filename, text)
+  File::open("#{filename}.txt", "a") do |file|
     file.sync = true
     file.puts text
   end
 end
 
-
-def following(client)
-  # followings = []
-  following_ids = client.friend_ids("taroooyan").to_a
-  begin
-    followings = following_ids.each_slice(100).to_a.inject ([]) do |users, ids|
-      users.concat(client.users(ids))
+class TwitterInfo
+  def initialize
+    Dotenv.load
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV["CONSUMER_KEY"]
+      config.consumer_secret = ENV["CONSUMER_SECRET"]
+      config.access_token = ENV["ACCESS_TOKEN"]
+      config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
     end
-    return followings
-  rescue Twitter::Error::TooManyRequests => error
-    sleep error.rate_limit.reset_in
-    retry
   end
-end
 
-
-def home_timeline(client, date_b)
-  begin
-    search = client.home_timeline(count: 100)
-    search.each do |tweet|
-      if date_b.strftime("%Y/%m/%d %X") < tweet.created_at.strftime("%Y/%m/%d %X")
-        text = "#{tweet.created_at.strftime("%Y/%m/%d %X")}: #{tweet.text}"
-        puts "TL #{text} :by @#{tweet.user.screen_name}"
-        save("@#{tweet.user.screen_name}", text)
+  def following
+    following_ids = @client.friend_ids.to_a
+    begin
+      followings = following_ids.each_slice(100).to_a.inject ([]) do |users, ids|
+        users.concat(@client.users(ids))
       end
+      return followings # Array
+    rescue Twitter::Error::TooManyRequests => error
+      sleep error.rate_limit.reset_in
+      retry
     end
-  rescue Twitter::Error::TooManyRequests => error
-    sleep error.rate_limit.reset_in
-    retry
   end
-end
 
-
-# フォロー中のユーザのtweetを100まで取得して保存する
-def initializetion(client)
-  followings = following(client)
-  followings.each_with_index do |user, i|
-    tweets = client.user_timeline(user.screen_name, { count: 100})
-    tweets.each do |tweet|
-      text = "#{tweet.created_at.strftime("%Y/%m/%d %X")}: #{tweet.text}"
-      save("@#{tweet.user.screen_name}", text)
+  # 自分のタイムラインを取得してuser別に保存 前回の取得した時間を引数にとる
+  def home_timeline(date_b)
+    begin
+      timelines = @client.home_timeline(count: 100)
+      timelines.each do |tweet|
+        if date_b.strftime("%Y/%m/%d %X") < tweet.created_at.strftime("%Y/%m/%d %X")
+          text = "#{tweet.created_at.strftime("%Y/%m/%d %X")}: #{tweet.text}"
+          puts "TL #{text} :by @#{tweet.user.screen_name}"
+          save(tweet.user.screen_name, text)
+        end
+      end
+    rescue Twitter::Error::TooManyRequests => error
+      sleep error.rate_limit.reset_in
+      retry
     end
-    progress_bar(i, followings.size)
-    sleep(60)
   end
-end
 
+  def find_dtweet(user)
+    tweets = Array.new
+    lines = Array.new # fileから読み込んだもの
+    delete_tweets = Array.new
+    ## userの100件のタイムラインをtweetsに格納
+    ## ツイートに改行が含まれている際の処理を改行で区切って対処することにする
+    user_all_info = @client.user_timeline(user.screen_name, { count: 100})
+    user_all_info.each do |user_info|
+      text = "#{user_info.created_at.strftime("%Y/%m/%d %X")}: #{user_info.text}".split("\n")
+      tweets.push(text)
+    end
+    tweets.flatten!
+    p tweets
+    ## /
 
-
-def find_dtweet(client, user)
-  tweets = Array.new
-  lines = Array.new
-  delete_tweets = Array.new
-  ## userの100件のタイムラインをtweetsに格納
-  ## ツイートに改行が含まれている際の処理を改行で区切って対処することにする
-  user_all_info = client.user_timeline(user.screen_name, { count: 100})
-  user_all_info.each do |user_info|
-    text = "#{user_info.created_at.strftime("%Y/%m/%d %X")}: #{user_info.text}".split("\n")
-    tweets.push(text)
-  end
-  tweets.flatten!
-  p tweets
-  ## /
-
-  ## ファイルから40行だけ逆順に一行ずつ読み込みlines格納
-  # ファイルが存在するかどうか
-  if File.exist?("@#{user.screen_name}.txt")
-    File::open("@#{user.screen_name}.txt", "r") do |file|
-      file_lines = file.each_line
-      file_lines.each_with_index do |line, i|
-        if i > 40
-          break
-        elsif line == "\n"
+    ## ファイルから逆順に一行ずつ読み込みlines格納し、linesの先頭100行を取る
+    ## ことでファイルから逆順で100行をlinesに格納することを実現している
+    # ファイルが存在するかどうか
+    if File.exist?("@#{user.screen_name}.txt")
+      file_lines = String.new
+      File::open("#{user.screen_name}.txt", "r") { |f| file_lines = f.read }
+      file_lines.split("\n").each do |line|
+        if line == "\n"
           next
         else
           lines.unshift(line.chomp)
         end
       end
-    end # /File::open
-  else
-    File::open("@#{user.screen_name}.txt", "w") do |file|
-    end
-  end # /File.exist
-  ## /
-  puts '-'*80
-  p lines
-  puts '-'*80
-  # ファイルにあってタイムラインに無いもののみを表示
-  p delete_tweets =  lines - tweets
-  delete_tweets.each do |delete_tweet|
-    notice_slack("#{delete_tweet} :by @#{user.screen_name}")
+      lines = lines.slice(0,99)
+    else
+      File::open("#{user.screen_name}.txt", "w") do |file|
+      end
+    end # /File.exist?
+    ## /
+
+    puts '-'*80
+    p lines
+    puts '-'*80
+    # ファイルにあってタイムラインに無いもののみを表示
+    p delete_tweets =  lines - tweets
+    # slackに一回で投稿するため
+    notice_slack("[Deleted] @#{user.screen_name}\n#{delete_tweets.join("\n")}")
+    puts '-'*80
   end
-  puts '-'*80
 end
 
-
 def main
-  Dotenv.load
-  client = Twitter::REST::Client.new do |config|
-    config.consumer_key = ENV["CONSUMER_KEY"]
-    config.consumer_secret = ENV["CONSUMER_SECRET"]
-    config.access_token = ENV["ACCESS_TOKEN"]
-    config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
-  end
-
   notice_slack("[Startup] twikeshigeter")
-  date_b = Time.now
-  following_users = following(client)
+  twitter = TwitterInfo.new()
+  date_before = Time.now
+  following_users = twitter.following
   # TLの取得回数
   count = 0
-  while true do
+  loop do
     following_users.each do |user|
-      find_dtweet(client, user)
+      twitter.find_dtweet(user)
       sleep(40)
-      date_a = Time.now
-      if (date_a-date_b).to_i > 60
-        home_timeline(client, date_b)
-        date_b = date_a
+      date_after = Time.now
+      if (date_after-date_before).to_i > 60
+        twitter.home_timeline(date_before)
+        date_before = date_after
         count += 1
-        notice_slack("[Running] #{count}回目の取得成功}") if count%60 == 0
+        notice_slack("[Running] #{count}回目のTL取得}") if count%60 == 0
       end
       sleep(30)
     end
-  end
+  end # /loop
 end
-
-
-def test
-  Dotenv.load
-  client = Twitter::REST::Client.new do |config|
-    config.consumer_key = ENV["CONSUMER_KEY"]
-    config.consumer_secret = ENV["CONSUMER_SECRET"]
-    config.access_token = ENV["ACCESS_TOKEN"]
-    config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
-  end
-  find_dtweet(client, client.user("USER"))
-  # initializetion(client)
-end
-
 
 main
-# test
-
